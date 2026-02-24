b1feb9c3ecac5d8a98147aa53e95f19f0fedfa24
Rahaman2024 <mahammadrahaman.shaik@vassarlabs.com>
2026-02-24 21:31:38 +0530

    Commented RabbitMQ dependencies

--- src/main/java/org/example/smarttaskmanager/config/RabbitMQConfig.java
+++ src/main/java/org/example/smarttaskmanager/config/RabbitMQConfig.java
@@ -1,13 +1,13 @@
-package org.example.smarttaskmanager.config;
-
-import org.springframework.amqp.core.Queue;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class RabbitMQConfig {
-    @Bean
-    public Queue taskQueue() {
-        return new Queue("task_notifications", false);
-    }
-}
+//package org.example.smarttaskmanager.config;
+//
+//import org.springframework.amqp.core.Queue;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class RabbitMQConfig {
+//    @Bean
+//    public Queue taskQueue() {
+//        return new Queue("task_notifications", false);
+//    }
+//}
